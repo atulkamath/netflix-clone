8a7cb8c92b3f786da9530231eec5ca8586fb2b5b
Atul <realatulkamath@gmail.com>
2021-10-20 12:20:03 +0400

    browse page hero video added

--- pages/browse.js
+++ pages/browse.js
@@ -2,8 +2,14 @@ import { useSession, signIn, signOut, getSession } from "next-auth/client";
 import { useRouter } from "next/router";
 import { Link } from "next/link";
 import BrowseHeader from "../components/BrowseHeader";
+import BrowseHero from "../components/BrowseHero";
 
 const browse = () => {
-  return <BrowseHeader />;
+  return (
+    <div>
+      <BrowseHeader />
+      <BrowseHero />
+    </div>
+  );
 };
 export default browse;
