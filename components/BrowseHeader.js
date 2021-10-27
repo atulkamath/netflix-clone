f2375075ce0d9a05a0a68440891515b10c47b484
atulkamath <realatulkamath@gmail.com>
2021-10-27 15:52:10 +0400

    Changes to browse header

--- components/BrowseHeader.js
+++ components/BrowseHeader.js
@@ -1,13 +1,26 @@
 import { BellIcon, SearchIcon } from "@heroicons/react/solid";
 import Image from "next/image";
-
+import { useState } from "react";
 const BrowseHeader = () => {
+  const [navbar, setNavbar] = useState("");
+  const changeNavBackground = () => {
+    if (window.scrollY >= 50) {
+      setNavbar(true);
+    } else setNavbar(false);
+  };
+  window.addEventListener("scroll", changeNavBackground);
   return (
-    <div className="flex px-4 bg-gradient-to-b from-black via-light-black to-transparent items-center justify-start  z-50 w-screen fixed">
+    <div
+      className={
+        navbar
+          ? "fixed z-50 flex items-center justify-start w-screen px-4 bg-gradient-to-b from-black via-black to-black transition-none duration-1000 ease-linear"
+          : "fixed z-50 flex items-center justify-start w-screen px-4 bg-gradient-to-b from-black via-light-black to-transparent"
+      }
+    >
       <div className="flex items-center justify-center mr-auto">
         <Image src="/netflix-3.svg" width={50} height={50} />
         <select
-          className=" cursor-pointer flex text-center text-white bg-transparent items-center justify-center text-xs ml-2"
+          className="flex items-center justify-center ml-2 text-xs text-center text-white bg-transparent cursor-pointer "
           name="language"
           id="language"
         >
