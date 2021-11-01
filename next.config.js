445a3ee6f3f4721dd5e9c107b7d1604d7c499dba
atulkamath <realatulkamath@gmail.com>
2021-11-02 01:05:28 +0400

    Browse grid added, api linked, and data population is active

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   images: {
-    domains: ["assets.nflxext.com", "occ-0-1723-92.1.nflxso.net"],
+    domains: ["assets.nflxext.com", "occ-0-1723-92.1.nflxso.net","image.tmdb.org","api.themoviedb.org"],
   },
 };
