699fe96c328a0d1aaaed3d7c5073adc96962dc41
atulkamath <realatulkamath@gmail.com>
2021-11-08 00:27:22 +0400

    Changees to menubar

--- pages/login.js
+++ pages/login.js
@@ -4,13 +4,11 @@ import Link from "next/link";
 import SignInCard from "../components/SignInCard";
 
 const login = () => {
-
-
   return (
-    <div className="bg-black md:bg-cover flex flex-col md:bg-banner">
+    <div className="flex flex-col bg-black md:bg-cover md:bg-banner">
       <div className="bg-black bg-opacity-80">
         <Link href="/">
-          <div className="mr-auto px-1 ml-4 mt-1">
+          <div className="px-1 mt-1 ml-4 mr-auto">
             <Image src="/netflix-3.svg" height={60} width={90} />
           </div>
         </Link>
